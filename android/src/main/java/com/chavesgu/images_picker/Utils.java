7b93309090114e4a5f57501b70e6eb946bfc5112
李萧萧 <lixiaoxiao@tcsl.com.cn>
2024-05-29 16:24:06 +0800

    Android 相册选择库使用最新版本v3.11.2项目可运行

--- android/src/main/java/com/chavesgu/images_picker/Utils.java
+++ android/src/main/java/com/chavesgu/images_picker/Utils.java
@@ -1,70 +1,54 @@
 package com.chavesgu.images_picker;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 
-import com.luck.picture.lib.PictureSelectionModel;
-import com.luck.picture.lib.PictureSelector;
-import com.luck.picture.lib.config.PictureConfig;
+import com.luck.picture.lib.basic.PictureSelectionCameraModel;
+import com.luck.picture.lib.basic.PictureSelectionModel;
 import com.luck.picture.lib.config.PictureMimeType;
+import com.luck.picture.lib.config.SelectModeConfig;
+import com.luck.picture.lib.engine.CompressFileEngine;
+import com.luck.picture.lib.interfaces.OnKeyValueResultCallbackListener;
 import com.luck.picture.lib.language.LanguageConfig;
-import com.yalantis.ucrop.view.OverlayView;
+import com.luck.picture.lib.utils.DateUtils;
 
-import java.lang.reflect.Array;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.io.File;
+import java.util.ArrayList;
+
+import top.zibin.luban.CompressionPredicate;
+import top.zibin.luban.Luban;
+import top.zibin.luban.OnNewCompressListener;
+import top.zibin.luban.OnRenameListener;
 
 public class Utils {
     public static PictureSelectionModel setPhotoSelectOpt(PictureSelectionModel model, int count, double quality) {
         model
-                .imageEngine(GlideEngine.createGlideEngine())
-                .maxSelectNum(count)
-                .minSelectNum(1)
-                .maxVideoSelectNum(count)
-                .minVideoSelectNum(1)
-                .selectionMode(count > 1 ? PictureConfig.MULTIPLE : PictureConfig.SINGLE)
-                .isSingleDirectReturn(false)
-                .isWeChatStyle(true)
-                .isCamera(false)
-                .isZoomAnim(true)
-                .isGif(true)
-                .isEnableCrop(false)
-                .isCompress(false)
-                .minimumCompressSize(100)
-                .isReturnEmpty(false)
-                .isAndroidQTransform(true)
-                .setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
-                .isOriginalImageControl(false)
+                .setImageEngine(GlideEngine.createGlideEngine())
+                .setMaxSelectNum(count)
+                .setMinSelectNum(1)
+                .setMaxVideoSelectNum(count)
+                .setMinVideoSelectNum(1)
+                .setSelectionMode(count > 1 ? SelectModeConfig.MULTIPLE : SelectModeConfig.SINGLE)
+                .isDirectReturnSingle(false)
+                .isDisplayCamera(false)
+                .isSelectZoomAnim(true)
+                .isGif(false)
+                ///.setCropEngine(new ImageFileCropEngine())
+                .setCompressEngine(new ImageFileCompressEngine())
+                .isEmptyResultReturn(false)
                 .isMaxSelectEnabledMask(true)
                 .setCameraImageFormat(PictureMimeType.JPEG)
                 .setCameraVideoFormat(PictureMimeType.MP4)
-                .renameCompressFile("image_picker_compress_"+UUID.randomUUID().toString()+".jpg")
-                .renameCropFileName("image_picker_crop_"+UUID.randomUUID().toString()+".jpg")
-//                .cameraFileName("image_picker_camera_"+UUID.randomUUID().toString()+".jpg")
+                .setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
         ;
-        if (quality > 0) {
-            model.isCompress(true).compressQuality((int) ((double) quality * 100));
-        }
         return model;
     }
-
-    public static PictureSelectionModel setCropOpt(PictureSelectionModel model, HashMap<String, Object> opt) {
+    public static PictureSelectionCameraModel setPhotoSelectOpt(PictureSelectionCameraModel model, int count, double quality) {
         model
-                .isEnableCrop(true)
-                .freeStyleCropMode(OverlayView.FREESTYLE_CROP_MODE_ENABLE)
-                .circleDimmedLayer(opt.get("cropType").equals("CropType.circle"))
-                .showCropFrame(!opt.get("cropType").equals("CropType.circle"))
-                .showCropGrid(false)
-                .rotateEnabled(true)
-                .scaleEnabled(true)
-                .isDragFrame(true)
-                .hideBottomControls(false)
-                .isMultipleSkipCrop(true)
-                .cutOutQuality((int) ((double) opt.get("quality") * 100));
-        if (opt.get("aspectRatioX") != null) {
-            model.isDragFrame(false);
-            model.withAspectRatio((int) opt.get("aspectRatioX"), (int) opt.get("aspectRatioY"));
-        }
+                .setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+                .setCameraImageFormat(PictureMimeType.JPEG)
+                .setCameraVideoFormat(PictureMimeType.MP4);
         return model;
     }
 
@@ -99,4 +83,81 @@ public class Utils {
         }
         return model;
     }
+
+    public static PictureSelectionCameraModel setLanguage(PictureSelectionCameraModel model, String language) {
+        switch (language) {
+            case "Language.Chinese":
+                model.setLanguage(LanguageConfig.CHINESE);
+                break;
+            case "Language.ChineseTraditional":
+                model.setLanguage(LanguageConfig.TRADITIONAL_CHINESE);
+                break;
+            case "Language.English":
+                model.setLanguage(LanguageConfig.ENGLISH);
+                break;
+            case "Language.Japanese":
+                model.setLanguage(LanguageConfig.JAPAN);
+                break;
+            case "Language.French":
+                model.setLanguage(LanguageConfig.FRANCE);
+                break;
+            case "Language.Korean":
+                model.setLanguage(LanguageConfig.KOREA);
+                break;
+            case "Language.German":
+                model.setLanguage(LanguageConfig.GERMANY);
+                break;
+            case "Language.Vietnamese":
+                model.setLanguage(LanguageConfig.VIETNAM);
+                break;
+            default:
+                model.setLanguage(-1);
+        }
+        return model;
+    }
+    /**
+     * 自定义压缩
+     */
+    private static class ImageFileCompressEngine implements CompressFileEngine {
+
+        @Override
+        public void onStartCompress(Context context, ArrayList<Uri> source, final OnKeyValueResultCallbackListener call) {
+            Luban.with(context).load(source).ignoreBy(100).setRenameListener(new OnRenameListener() {
+                @Override
+                public String rename(String filePath) {
+                    int indexOf = filePath.lastIndexOf(".");
+                    String postfix = indexOf != -1 ? filePath.substring(indexOf) : ".jpg";
+                    return DateUtils.getCreateFileName("CMP_") + postfix;
+                }
+            }).filter(new CompressionPredicate() {
+                @Override
+                public boolean apply(String path) {
+                    if (PictureMimeType.isUrlHasImage(path) && !PictureMimeType.isHasHttp(path)) {
+                        return true;
+                    }
+                    return !PictureMimeType.isUrlHasGif(path);
+                }
+            }).setCompressListener(new OnNewCompressListener() {
+                @Override
+                public void onStart() {
+
+                }
+
+                @Override
+                public void onSuccess(String source, File compressFile) {
+                    if (call != null) {
+                        call.onCallback(source, compressFile.getAbsolutePath());
+                    }
+                }
+
+                @Override
+                public void onError(String source, Throwable e) {
+                    if (call != null) {
+                        call.onCallback(source, null);
+                    }
+                }
+            }).launch();
+        }
+    }
+
 }
